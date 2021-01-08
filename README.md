b5c032ffcd1ea5f24644843e98fe413eabbe29b1
Timon <timonnickel@web.de>
2021-01-08 19:03:12 +0100

    add new lines to strings, edit README
    Improve readability of printed feedback.

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# What this program can do:
-    - [x] Deal with invalid input (out of bounds numbers and non-integers) - [x] Provide instant feedback using F-Strings
+In this game you are asked to guess a random number in the range of 1 to 100. If the guessed number does not match the generated number, the program will provide you with feedback if the number is less or greater than your guess.
 
-# TODO:
-    - [ ] Make bounds for random number configurable by player
-    - [ ] Implement a graphical user interface
+### What this program can do:
+    - [x] Deal with invalid input (out of bounds numbers and non-integers)
+    - [x] Provide instant feedback of user input
--- guessing_game.py
+++ guessing_game.py
@@ -3,14 +3,16 @@ import random as rnd
 random_number = rnd.randint(1, 100)
 is_correct = False
 
+# remove comment to print random number
 print(random_number)
-print("Please guess a full number between 1 and 100.")
+
+print("Please guess a full number between 1 and 100. \nIf you wish to stop playing, press Ctrl+C.")
 
 while is_correct == False:
     user_input = input("Input: ")
     try:
         if int(user_input) == random_number:
-            print(F"{user_input} is correct!")
+            print(F"Well done! \n{user_input} is correct!")
             is_correct = True
         elif int(user_input) < 1 or int(user_input) > 100:
             print(F"{user_input} is out of bounds (1 - 100)")
